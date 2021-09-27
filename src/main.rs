ecbb89b0d08823570262f9fcaff951522fcd9565
Hao Shi <shisoftgenius@gmail.com>
2021-09-27 12:47:33 -0400

    scaffolding with CLR

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{App, Arg, SubCommand};
+
+const PORTS: &'static str = "ports";
+const BRIDGE: &'static str = "bridge";
+const THREADS: &'static str = "threads";
+const BINDING: &'static str = "binding";
+
+#[tokio::main]
+async fn main() {
+    let matches = App::new("HomeBridge by Shisoft")
+        .version("0.1")
+        .author("Hao Shi <shisoftgenius@gmail.com>")
+        .subcommand(
+            SubCommand::with_name("server")
+                .about("run at server mode at the machine you want to expose")
+                .arg(Arg::with_name(PORTS)
+                    .short("p")
+                    .help("provide ports mapping to expose, in format like 111:222,333:444, where right hand side is the port on bridge server"))
+                .arg(Arg::with_name(BRIDGE)
+                    .short("b")
+                    .help("the address to the bridge"))
+                .arg(Arg::with_name("threads")
+                    .short("t")
+                    .help("number of threads per ports")),
+        )
+        .subcommand(SubCommand::with_name(BRIDGE)
+                .about("run at bridge mode at the machine have fixed IP address and controllable firewall")
+                .arg(Arg::with_name(BINDING)
+                    .short("b")
+                    .help("address for server to talk to")))
+        .get_matches();
+    if let Some(m) = matches.subcommand_matches("server") {
+        let ports = m.value_of(PORTS).expect("Must provide ports to expose");
+        let bridge = m.value_of(BRIDGE).expect("Must provide bridge address");
+        let threads = m.value_of(THREADS).unwrap_or("1").parse::<u32>().expect("threads should be a number");
+        start_server(ports, bridge, threads).await;
+    } else if let Some(m) = matches.subcommand_matches(BRIDGE) {
+        let bind = m.value_of(BINDING).unwrap_or("0.0.0.0:5678");
+        start_bridge(bind).await;
+    } else {
+        panic!("Don't know how to run");
+    }
+}
+
+async fn start_server<'a>(ports: &'a str, bridge: &'a str, threads: u32) {
+
+}
+
+async fn start_bridge<'a>(bind: &'a str) {
+
 }
