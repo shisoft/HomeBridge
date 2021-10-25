a1905e219835fa292dcc0e92e70cca18833158f5
Hao Shi <shisoftgenius@gmail.com>
2021-10-25 03:17:31 -0400

    still storing out

--- src/bridge.rs
+++ src/bridge.rs
@@ -5,13 +5,17 @@ use lightning::map::{Map, ObjectMap};
 use log::*;
 use parking_lot::RwLock;
 use std::error::Error;
+use std::io;
+use std::net::SocketAddr;
 use std::sync::{
     atomic::{AtomicU64, AtomicUsize},
     Arc,
 };
-use tokio::net::TcpListener;
-use tokio::sync::mpsc::{self, Sender};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc::{self, channel, Sender};
 use tokio_util::codec::{BytesCodec, Framed};
+
+use crate::server;
 
 struct ServerConnection {
     id: u64,
@@ -24,25 +28,25 @@ struct PortServerConnections {
 }
 
 struct BridgePorts {
-    conns: ObjectMap<Arc<ObjectMap<Arc<RwLock<PortServerConnections>>>>>
+    conns: ObjectMap<Arc<ObjectMap<Arc<RwLock<PortServerConnections>>>>>,
 }
 
 struct BridgeServers {
-    conns: ObjectMap<Arc<ServerConnection>>
+    conns: ObjectMap<Arc<ServerConnection>>,
 }
 
 struct Bridge {
     ports: BridgePorts,
     servs: BridgeServers,
+    clients: ObjectMap<Arc<ClientConnection>>,
     conn_counter: AtomicU64,
-    serv_vounter: AtomicU64
+    serv_counter: AtomicU64,
 }
 
 struct ClientConnection {
-
+    id: u64,
+    tx: Sender<BytesMut>,
 }
-
-
 
 struct ServerConnPool {}
 
@@ -56,115 +60,31 @@ pub async fn new<'a>(addr: &'a str) -> Result<(), Box<dyn Error>> {
         // Spawn our handler to be run asynchronously.
         debug!("Accepted bridge connection from {:?}", &addr);
         let bridge = bridge.clone();
+        let serv_id = bridge
+            .serv_counter
+            .fetch_add(1, std::sync::atomic::Ordering::AcqRel);
         tokio::spawn(async move {
-            let transport = Framed::new(stream, BytesCodec::new());
-            let (mut writer, mut reader) = transport.split();
-            let mut num_ports_bytes = reader.next().await.unwrap().unwrap();
-            let mut num_ports_data = [0u8; 8];
-            num_ports_bytes.copy_to_slice(&mut num_ports_data);
-            let num_ports = u64::from_le_bytes(num_ports_data);
-            let mut ports = Vec::with_capacity(num_ports as usize);
-            let serv_id = bridge_serv_counter.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
-            for _ in 0..num_ports {
-                let mut port_bytes = reader.next().await.unwrap().unwrap();
-                let mut port_data = [0u8; 4];
-                port_bytes.copy_to_slice(&mut port_data);
-                ports.push(u32::from_le_bytes(port_data));
-            }
-            info!(
-                "Server {:?} requesting {} ports {:?}",
-                addr, num_ports, ports
-            );
-            let (write_tx, mut write_rx) = mpsc::channel::<(u32, u64, BytesMut)>(128);
-            tokio::spawn(async move {
-                while let Some((port, conn, data)) = write_rx.recv().await {
-                    let data_size = data.len();
-                    let mut out_data = BytesMut::new();
-                    out_data.put_u32_le(port);
-                    out_data.put_u64_le(conn);
-                    out_data.put(data);
-                    if let Err(e) = writer.send(out_data.freeze()).await {
-                        error!(
-                            "Error on sending packets to server, port {}, conn {}, data size {}",
-                            port, conn, data_size
-                        );
-                    }
-                }
-            });
-            while let Some(Ok(mut res)) = reader.next().await {
-                let conn_id = res.get_u64_le();
-                if let Some(conn) = bridge_servs.get(&(conn_id as usize)) {
-                    // TODO: redirect packets to the connection for clients
-                } else {
-                    error!("Received packet from conn {} but cannot find it", conn_id);
-                }
-            }
-            for port in ports {
-                let port_key = port as usize;
-                loop {
-                    if let Some(servs) = bridge_ports.get(&port_key) {
-                        servs.write().add_server_conn(serv_id);
-                        info!("Added new port {} with serv_id {}", port, serv_id);
-                        break;
-                    } else {
-                        let servs = Arc::new(RwLock::new(Ports::new()));
-                        if bridge_ports.try_insert(&port_key, servs.clone()).is_none() {
-                            servs.write().add_server_conn(serv_id);
-                            info!("Starting to listen at port {}", port);
-                            match TcpListener::bind(format!("0.0.0.0:{}", port)).await {
-                                Ok(listener) => {
-                                    match listener.accept().await {
-                                        Ok((stream, addr)) => {
-                                            let conn_id = bridge_conn_counter.fetch_add(1, AcqRel);
-                                            // let conn_chan = channel(128);
-                                            trace!(
-                                                "Accepting connection {} at port {} from {:?}",
-                                                conn_id,
-                                                port,
-                                                addr
-                                            );
-                                            let servs = servs.clone();
-                                            let bridge_servs = bridge_servs.clone();
-                                            tokio::spawn(async move {
-                                                let transport =
-                                                    Framed::new(stream, BytesCodec::new());
-                                                let (mut writer, mut reader) = transport.split();
-                                                while let Some(Ok(mut res)) = reader.next().await {
-                                                    let serv_id = servs.read().next_server_conn();
-                                                    loop {
-                                                        match bridge_servs.get(&(serv_id as usize))
-                                                        {
-                                                            Some(serv) => {
-                                                                serv.sender
-                                                                    .send((port, conn_id, res))
-                                                                    .await;
-                                                                break;
-                                                            }
-                                                            None => {
-                                                                error!("Cannot find service with id {}", serv_id);
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            });
-                                        }
-                                        Err(e) => {
-                                            error!(
-                                                "Cannot accept connection at port {}, error {:?}",
-                                                port, e
-                                            );
-                                        }
-                                    }
-                                }
-                                Err(e) => {
-                                    error!("Cannot listen at port {}, error {:?}", port, e);
-                                }
-                            }
-                        }
-                    }
-                    warn!("Need to retry adding port {} with {}", port, serv_id);
-                }
-            }
+            bridge
+                .servs
+                .new_server(&bridge, serv_id, stream, addr)
+                .await;
+            // for port in ports {
+            //     let port_key = port as usize;
+            //     loop {
+            //         if let Some(servs) = bridge_ports.get(&port_key) {
+            //             servs.write().add_server_conn(serv_id);
+            //             info!("Added new port {} with serv_id {}", port, serv_id);
+            //             break;
+            //         } else {
+            //             let servs = Arc::new(RwLock::new(Ports::new()));
+            //             if bridge_ports.try_insert(&port_key, servs.clone()).is_none() {
+            //                 servs.write().add_server_conn(serv_id);
+
+            //             }
+            //         }
+            //         warn!("Need to retry adding port {} with {}", port, serv_id);
+            //     }
+            // }
         });
     }
 }
@@ -192,9 +112,10 @@ impl Bridge {
     pub fn instance() -> Self {
         Self {
             conn_counter: AtomicU64::new(0),
-            serv_vounter: AtomicU64::new(0),
+            serv_counter: AtomicU64::new(0),
             ports: BridgePorts::new(),
-            servs: BridgeServers::new()
+            servs: BridgeServers::new(),
+            clients: ObjectMap::with_capacity(128),
         }
     }
 }
@@ -202,7 +123,7 @@ impl Bridge {
 impl BridgePorts {
     fn new() -> Self {
         Self {
-            conns: ObjectMap::with_capacity(64)
+            conns: ObjectMap::with_capacity(64),
         }
     }
 }
@@ -210,7 +131,132 @@ impl BridgePorts {
 impl BridgeServers {
     fn new() -> Self {
         Self {
-            conns: ObjectMap::with_capacity(32)
+            conns: ObjectMap::with_capacity(32),
         }
+    }
+
+    async fn new_server(
+        &self,
+        bridge: &Arc<Bridge>,
+        serv_id: u64,
+        stream: TcpStream,
+        addr: SocketAddr,
+    ) {
+        let server_conn = ServerConnection::new(bridge, stream, addr).await;
+        info!("New server connection {:?}, id {}", addr, serv_id);
+        self.conns
+            .insert(&(serv_id as usize), Arc::new(server_conn));
+    }
+}
+
+impl ServerConnection {
+    async fn new(bridge: &Arc<Bridge>, stream: TcpStream, addr: SocketAddr) -> Self {
+        let (write, ports) = Self::init_connection(bridge, stream).await;
+        
+        unimplemented!()
+    }
+
+    async fn init_connection(
+        bridge: &Arc<Bridge>,
+        stream: TcpStream,
+    ) -> (Sender<(u32, u64, BytesMut)>, Vec<u32>) {
+        let bridge = bridge.clone();
+        let transport = Framed::new(stream, BytesCodec::new());
+        let (mut writer, mut reader) = transport.split();
+        let mut num_ports_bytes = reader.next().await.unwrap().unwrap();
+        let mut num_ports_data = [0u8; 8];
+        num_ports_bytes.copy_to_slice(&mut num_ports_data);
+        let num_ports = u64::from_le_bytes(num_ports_data);
+        let mut ports = Vec::with_capacity(num_ports as usize);
+        for _ in 0..num_ports {
+            let mut port_bytes = reader.next().await.unwrap().unwrap();
+            let mut port_data = [0u8; 4];
+            port_bytes.copy_to_slice(&mut port_data);
+            ports.push(u32::from_le_bytes(port_data));
+        }
+        let (write_tx, mut write_rx) = mpsc::channel::<(u32, u64, BytesMut)>(128);
+        // Receving packets sending through the connection to the server
+        tokio::spawn(async move {
+            while let Some((port, conn, data)) = write_rx.recv().await {
+                let data_size = data.len();
+                let mut out_data = BytesMut::new();
+                out_data.put_u32_le(port);
+                out_data.put_u64_le(conn);
+                out_data.put(data);
+                if let Err(e) = writer.send(out_data.freeze()).await {
+                    error!(
+                        "Error on sending packets to server, port {}, conn {}, data size {}",
+                        port, conn, data_size
+                    );
+                }
+            }
+        });
+        // Receving packets sent from the server to some client
+        tokio::spawn(async move {
+            while let Some(Ok(mut res)) = reader.next().await {
+                let conn_id = res.get_u64_le();
+                if let Some(conn) = bridge.clients.get(&(conn_id as usize)) {
+                    conn.tx.send(res).await;
+                } else {
+                    error!("Received packet from conn {} but cannot find it", conn_id);
+                }
+            }
+        });
+        return (write_tx, ports);
+    }
+}
+
+async fn init_client_server(
+    port: u32,
+    bridge: &Arc<Bridge>,
+    port_conns: &Arc<RwLock<PortServerConnections>>,
+) -> io::Result<()> {
+    info!("Starting to listen at port {}", port);
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        let bridge = bridge.clone();
+        let port_conns = port_conns.clone();
+        let conn_id = bridge.conn_counter.fetch_add(1, AcqRel);
+        let (client_tx, mut client_rx) = channel::<BytesMut>(128);
+        bridge.clients.insert(
+            &(conn_id as usize),
+            Arc::new(ClientConnection {
+                id: conn_id,
+                tx: client_tx,
+            }),
+        );
+        tokio::spawn(async move {
+            trace!(
+                "Accepting connection {} at port {} from {:?}",
+                conn_id,
+                port,
+                addr
+            );
+            let transport = Framed::new(stream, BytesCodec::new());
+            let (mut writer, mut reader) = transport.split();
+            tokio::spawn(async move {
+                while let Some(data) = client_rx.recv().await {
+                    writer.send(data.freeze()).await;
+                }
+                writer.close().await;
+            });
+            while let Some(Ok(res)) = reader.next().await {
+                let serv_id = port_conns.read().next_server_conn();
+                loop {
+                    match bridge.servs.conns.get(&(serv_id as usize)) {
+                        Some(serv) => {
+                            serv.sender.send((port, conn_id, res)).await;
+                            break;
+                        }
+                        None => {
+                            error!("Cannot find service with id {}", serv_id);
+                        }
+                    }
+                }
+            }
+            bridge.clients.remove(&(conn_id as usize));
+            info!("Connection {} of port {} disconnected", conn_id, port);
+        });
     }
 }
