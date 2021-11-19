96281d80f8902db941f9b1ab7530c4d9ce6b83d5
Hao Shi <shisoftgenius@gmail.com>
2021-11-18 21:39:10 -0500

    should sleep

--- src/server.rs
+++ src/server.rs
@@ -249,6 +249,7 @@ impl Connection {
                 trace!("Sending heartbeat packet as {}", id);
                 close_tx.send(buf.freeze()).await.unwrap();
             }
+            tokio::time::sleep(Duration::from_secs(5)).await;
         });
         tokio::spawn(async move {
             while let Some(data) = host_rx.recv().await {
